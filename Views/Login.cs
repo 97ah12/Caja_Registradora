0ad9af2db7fb74751999e930565b1ced4e6ccc7f
oscar calle <elcalle97@outlook.es>
2021-10-21 21:02:48 -0500

    Diseño login v.#1

--- Views/Login.cs
+++ Views/Login.cs
@@ -16,5 +16,10 @@ namespace Caja_Registradora.Views
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
