75ba572bd7da80e46de3c880e92b63c7fd65ad80
StefannyHiguita <yulisa.higuita602@pascualbravo.edu.co>
2021-10-26 01:38:36 -0500

    Create ProductDAO, ProductDTO

--- DTO/LoginDTO.cs
+++ DTO/LoginDTO.cs
@@ -10,17 +10,25 @@ namespace Caja_Registradora.DTO
 {
     public class LoginDTO
     {
+        //Declaracion de clase DAO y el modelo user
         readonly LoginDAO _objDAO;
         User _user;
+        //Constructor
         public LoginDTO()
         {
+            //Instanciando las clases declaradas
             _objDAO = new();
             _user = new();
         }
-
+        //Metodo de comparacion de usuario y contraseña
         public bool LoginUser(User user)
         {
+            //Llamado del metodo user de la clase DAO, que retorna user(Modelo)
             _user = _objDAO.LoginUser();
+            /*Se valida si usuario y contraseña que viene de DAO coincide 
+            con el parametro del usuario enviado desde la vista y retorna TRUE si la comparacion es Correcta,
+            FALSE si algun dato no coincide
+            */
             if (user.UserName == _user.UserName && user.Password == _user.Password)
                 return true;
             else
