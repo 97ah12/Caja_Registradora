4e086318f66bf4979382dddefc4471d29a5316a6
StefannyHiguita <yulisa.higuita602@pascualbravo.edu.co>
2021-10-25 23:14:27 -0500

    Get y set de Product

--- Models/Product.cs
+++ Models/Product.cs
@@ -8,5 +8,9 @@ namespace Caja_Registradora.Models
 {
     class Product
     {
+        public String Code { get; set;}
+        public String Description{ get; set;}
+        public int Quantity { get; set; }
+        public float MyProperty { get; set; }
     }
 }
